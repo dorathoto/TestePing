b7c40f69532d34758251f6b84b9b1f8b715012c6
Leonardo Dorathoto <1705323@aluno.univesp.br>
2021-04-04 12:09:23 -0300

    Add project files.

---
+++ TestePing/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestePing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // PingByMicrosoft();
+            var retorno = EstaOnlineByGoogle();
+        }
+
+        private static void PingByMicrosoft()
+        {
+            Ping pingSender = new Ping();
+            pingSender.PingCompleted += new PingCompletedEventHandler(PingCompletedCallback);
+            PingOptions options = new PingOptions();
+
+            String host = "8.8.8.8";
+            // Use the default Ttl value which is 128,
+            // but change the fragmentation behavior.
+            options.DontFragment = true;
+
+            // Create a buffer of 32 bytes of data to be transmitted.
+            string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            byte[] buffer2 = new byte[32];
+            int timeout = 120;
+            PingReply reply = pingSender.Send(host, timeout, buffer2, options);
+            if (reply.Status == IPStatus.Success)
+            {
+                Console.WriteLine("Address: {0}", reply.Address.ToString());
+                Console.WriteLine("RoundTrip time: {0}", reply.RoundtripTime);
+                Console.WriteLine("Time to live: {0}", reply.Options.Ttl);
+                Console.WriteLine("Don't fragment: {0}", reply.Options.DontFragment);
+                Console.WriteLine("Buffer size: {0}", reply.Buffer.Length);
+            }
+        }
+
+        private static bool EstaOnlineByGoogle()
+        {
+
+            try
+            {
+                Ping myPing = new Ping();
+                String host = "8.8.8.8";
+                byte[] buffer = new byte[32];
+                int timeout = 3500;
+                PingOptions pingOptions = new PingOptions();
+                PingReply reply = myPing.Send(host, timeout, buffer, pingOptions);
+                if (reply.Status == IPStatus.Success)
+                {
+                    Console.WriteLine($"        Status: Online");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine($"        Status: OFFLINE - {reply.Status}");
+                    return false;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("        ERRO: {0}", ex.Message);
+
+                return false;
+            }
+            finally
+            {
+                Console.WriteLine($"-------- fim => EstaOnlineByGoogle");
+            }
+        }
+
+        private static void PingCompletedCallback(object sender, PingCompletedEventArgs e)
+        {
+            // If the operation was canceled, display a message to the user.
+            if (e.Cancelled)
+            {
+                Console.WriteLine("Ping canceled.");
+
+                // Let the main thread resume.
+                // UserToken is the AutoResetEvent object that the main thread
+                // is waiting for.
+                ((AutoResetEvent)e.UserState).Set();
+            }
+
+            // If an error occurred, display the exception to the user.
+            if (e.Error != null)
+            {
+                Console.WriteLine("Ping failed:");
+                Console.WriteLine(e.Error.ToString());
+
+                // Let the main thread resume.
+                ((AutoResetEvent)e.UserState).Set();
+            }
+
+            PingReply reply = e.Reply;
+
+            DisplayReply(reply);
+
+            // Let the main thread resume.
+            ((AutoResetEvent)e.UserState).Set();
+        }
+
+        public static void DisplayReply(PingReply reply)
+        {
+            if (reply == null)
+                return;
+
+            Console.WriteLine("ping status: {0}", reply.Status);
+            if (reply.Status == IPStatus.Success)
+            {
+                Console.WriteLine("Address: {0}", reply.Address.ToString());
+                Console.WriteLine("RoundTrip time: {0}", reply.RoundtripTime);
+                Console.WriteLine("Time to live: {0}", reply.Options.Ttl);
+                Console.WriteLine("Don't fragment: {0}", reply.Options.DontFragment);
+                Console.WriteLine("Buffer size: {0}", reply.Buffer.Length);
+            }
+        }
+    }
+}
